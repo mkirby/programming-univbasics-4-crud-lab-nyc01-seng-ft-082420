e7f4b73b026a23566d51baed2b3433c46637460d
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-13 14:38:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,7 @@ end
 
 def retrieve_last_element_from_array(array)
   #array[-1]
-  array[array.length -1]
+  array[array.length - 1]
 end
 
 def update_element_from_index(array, index_number, element)
